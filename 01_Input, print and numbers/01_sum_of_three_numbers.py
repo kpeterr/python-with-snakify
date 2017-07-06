94a3152a5d8e742aaca963b1a0a181eebe9f0b08
kpeterr <kpeterr2@gmail.com>
2017-07-06 21:05:23 +0200

    Create First Program ;-)

---
+++ 01_Input, print and numbers/01_sum_of_three_numbers.py
@@ -0,0 +1,7 @@
+print("Enter 3 numbers")
+
+a = int(input("First number:"))
+b = int(input("Second number:"))
+c = int(input("Third number:"))
+
+print("The sum of the numbers:" + str(a) + str(b) + str(c))
